5ef6b57911723ba99abc5c4c2a8a2641605ae17f
Anton-Malyugin <a.malyugin@mute-lab.com>
2018-04-04 09:03:47 +0400

    add ArticleList

--- src/Article.js
+++ src/Article.js
@@ -32,7 +32,9 @@ export default class Article extends Component {
     return <section>{article.text}</section>
   }
 
-  toggleOpen = () => {
+  toggleOpen = (ev) => {
+    ev.preventDefault()
+    console.log('---', ev.nativeEvent)
     this.setState({
       isOpen: !this.state.isOpen
     })
