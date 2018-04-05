44111bf3c3b40dd24d3c73595913d11a65430093
Anton-Malyugin <a.malyugin@mute-lab.com>
2018-04-05 13:42:58 +0400

    HW1 Done my version

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Comment from './Comment';
 
-export default function CommentList({comments}) {
-  const commentElement = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment} /></li>)
+export default class CommentList extends Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <ul>{commentElement}</ul>
-  )
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  render() {
+    const {isOpen} = this.state
+    return (
+      <div>
+        <button onClick = {this.toggleOpen}>{isOpen ? 'Скрыть' : 'Показать'}</button>
+        <div>{this.getBody()}</div>
+      </div>
+    )
+  }
+
+  toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  getBody() {
+    if(!this.state.isOpen) return null
+
+    const {comments} = this.props
+    if(!comments || comments.length) return <p>No comments</p>
+
+    const commentElement = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment} /></li>)
+
+    return (
+      <ul>{commentElement}</ul>
+    )
+  }
 }
+
+// export default function CommentList({comments}) {
+//   const commentElement = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment} /></li>)
+//
+//   return (
+//     <ul>{commentElement}</ul>
+//   )
+// }
